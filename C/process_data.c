5d47ead432cee1c9fb2290ecceef1cfa12619f43
yxj <564197835@qq.com>
2022-11-09 14:03:21 +0800

    use adjustable parameters in IncreasePitch

--- C/process_data.c
+++ C/process_data.c
@@ -18,9 +18,9 @@ short* TxPong = TxBuffer + 2 * H_I;
 // -----
 
 fract16 resampled_buffer_right[F_A] = {0};
-int resampled_buffer_input_position = F_A - H_A;
+int resampled_buffer_input_position_right = F_A - H_A;
 fract16 stretched_buffer_right[F_S] = {0};
-int stretched_buffer_output_position = F_S - H_S;
+int stretched_buffer_output_position_right = F_S - H_S;
 
 void Pass(const fract16 x[], fract16 y[], int n, int step) {
   for (int i = 0; i < n; i++) {
@@ -29,7 +29,9 @@ void Pass(const fract16 x[], fract16 y[], int n, int step) {
 }
 
 void IncreasePitch(const fract16 x[], fract16 y[], fract16 resampled_buffer[],
-                   fract16 stretched_buffer[], int step) {
+                   int* resampled_buffer_input_position,
+                   fract16 stretched_buffer[],
+                   int* stretched_buffer_output_position, int step) {
   // 插值重采样
   fract16 resampled_data[H_A] = {0};
   for (int i = 0; i < H_A; i++) {
@@ -41,7 +43,7 @@ void IncreasePitch(const fract16 x[], fract16 y[], fract16 resampled_buffer[],
         (1.0 - offset) * x[step * left] + offset * x[step * right];
   }
   // 拷贝差值重采样的数据
-  int resampled_buffer_copy_position = resampled_buffer_input_position;
+  int resampled_buffer_copy_position = *resampled_buffer_input_position;
   for (int i = 0; i < H_A; i++) {
     resampled_buffer[resampled_buffer_copy_position] = resampled_data[i];
     resampled_buffer_copy_position += 1;
@@ -49,12 +51,12 @@ void IncreasePitch(const fract16 x[], fract16 y[], fract16 resampled_buffer[],
       resampled_buffer_copy_position = 0;
     }
   }
-  resampled_buffer_input_position += H_A;  // 这里需要做循环处理
-  if (resampled_buffer_input_position >= F_A) {
-    resampled_buffer_input_position -= F_A;
+  *resampled_buffer_input_position += H_A;  // 这里需要做循环处理
+  if (*resampled_buffer_input_position >= F_A) {
+    *resampled_buffer_input_position -= F_A;
   }
   // 乘窗函数
-  int resampled_start_position = resampled_buffer_input_position;
+  int resampled_start_position = *resampled_buffer_input_position;
   fract16 dot_production_result[N] = {0};
   for (int i = 0; i < N; i++) {
     int temp = resampled_buffer[resampled_start_position] * w[i];  // 32位
@@ -67,19 +69,19 @@ void IncreasePitch(const fract16 x[], fract16 y[], fract16 resampled_buffer[],
   // 叠加和输出
   for (int i = 0; i < H_S; i++) {
     // 输出
-    y[step * i] = stretched_buffer[stretched_buffer_output_position + i] +
+    y[step * i] = stretched_buffer[*stretched_buffer_output_position + i] +
                   dot_production_result[i];
     // 已经输出了所以置零
-    stretched_buffer[stretched_buffer_output_position + i] = 0;
+    stretched_buffer[*stretched_buffer_output_position + i] = 0;
   }
-  if (stretched_buffer_output_position == 0) {
-    stretched_buffer_output_position = H_S;
+  if (*stretched_buffer_output_position == 0) {
+    *stretched_buffer_output_position = H_S;
   } else {
-    stretched_buffer_output_position = 0;
+    *stretched_buffer_output_position = 0;
   }
   for (int i = 0; i < H_S; i++) {
     // 需要存储给下一次加和的值
-    stretched_buffer[stretched_buffer_output_position + i] =
+    stretched_buffer[*stretched_buffer_output_position + i] =
         dot_production_result[H_S + i];
   }
   return;
@@ -92,17 +94,21 @@ void Process_Data(void) {
   // Ping-Pong Flag
   static int ping = 0;
   /* core processing in ping-pong mode */
-  if (ping == 0) { // left and right channels filtering, ping slot
+  if (ping == 0) {  // left and right channels filtering, ping slot
     // Pass(RxPing+0, TxPing+0, H_I, 2);
     // Pass(RxPing+1, TxPing+1, H_I, 2);
     IncreasePitch(RxPing + 0, TxPing + 0, resampled_buffer_right,
-                  stretched_buffer_right, 2);
+                  &resampled_buffer_input_position_right,
+                  stretched_buffer_right,
+                  &stretched_buffer_output_position_right, 2);
     // IncreasePitch(RxPing + 1, TxPing + 1, 2);
-  } else { // left and right channels filtering, pong slot
+  } else {  // left and right channels filtering, pong slot
     // Pass(RxPong+0, TxPong+0, H_I, 2);
     // Pass(RxPong+1, TxPong+1, H_I, 2);
     IncreasePitch(RxPong + 0, TxPong + 0, resampled_buffer_right,
-                  stretched_buffer_right, 2);
+                  &resampled_buffer_input_position_right,
+                  stretched_buffer_right,
+                  &stretched_buffer_output_position_right, 2);
     // IncreasePitch(RxPong + 1, TxPong + 1, 2);
   }
   ping ^= 0x1;
